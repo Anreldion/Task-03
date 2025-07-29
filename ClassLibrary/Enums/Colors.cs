e1e72707305bf9123038463b2b0eb9efa90e04f2
asamusenkaEPAM <asamusenka@idbs.com>
2025-07-29 11:31:59 +0200

    Colors has been separated.

---
+++ ClassLibrary/Enums/Colors.cs
@@ -0,0 +1,14 @@
+﻿namespace Shapes.Enums
+{
+    public enum Colors
+    {
+        None = 0,
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        LightBlue,
+        Violet,
+    }
+}
