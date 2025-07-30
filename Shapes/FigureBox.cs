e9ca37578b549a50947c3e5e97075f17c6bad571
asamusenkaEPAM <asamusenka@idbs.com>
2025-07-30 20:32:36 +0200

    Fix some bugs and comments has been added.

--- Shapes/FigureBox.cs
+++ Shapes/FigureBox.cs
@@ -2,56 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using Shapes.Exceptions;
+using Shapes.Extensions;
 using Shapes.Services.Interfaces;
 using Shapes.Shapes;
 using Shapes.Sheets;
 
 namespace Shapes
 {
+    /// <summary>
+    /// Represents a container for storing and managing geometric shapes.
+    /// Provides operations for adding, removing, replacing, filtering, and saving/loading figures.
+    /// </summary>
     public class FigureBox
     {
-        private const int MaxCount = 20; 
+        /// <summary>
+        /// The maximum number of figures allowed in the box.
+        /// </summary>
+        public const int MaxCapacity = 20;
+
         private const double Tolerance = 0.01;
-        private readonly IWriterService _writerService;
-        private List<Shape> _figures = [];
+        private readonly ISaveLoadService _saveLoadService;
+        private readonly List<Shape> _figures = [];
 
-        public FigureBox(IWriterService writerService)
+        /// <summary>
+        /// Gets the current number of figures in the box.
+        /// </summary>
+        public int Count => _figures.Count;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FigureBox"/> class using the given persistence service.
+        /// </summary>
+        /// <param name="saveLoadService">The service used for saving and loading figures.</param>
+        public FigureBox(ISaveLoadService saveLoadService)
         {
-            _writerService = writerService;
-        }
-        
-        public bool AddFigure(Shape shape)
-        {
-            if (_figures.Count >= MaxCount)
-            {
-                throw new IndexOutOfRangeException();
-            }
-
-            if (_figures.Contains(shape))
-            {
-                throw new BoxFullException($"FigureBox already contains {shape.GetType()}");
-            }
-
-            _figures.Add(shape);
-            return true;
-        }
-
-        public Shape GetFigureByIndex(int index)
-        {
-            if (index < 0 || _figures.Count <= index)
-            {
-                throw new FigureNotFoundException($"The requested number ({index}) exceeds the number of items in the collection!");
-            }
-
-            return _figures[index];
-
+            _saveLoadService = saveLoadService;
         }
 
         /// <summary>
-        /// Извлечь фигуру по номеру (фигура удаляется из коробки)
+        /// Adds a new shape to the box.
         /// </summary>
-        /// <param name="index"></param>
-        /// <returns></returns>
+        /// <param name="shape">The shape to add.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown if the box exceeds <see cref="MaxCapacity"/>.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the shape already exists in the box.</exception>
+        public void AddFigure(Shape shape)
+        {
+            if (_figures.Count >= MaxCapacity)
+                throw new IndexOutOfRangeException("The box is full.");
+
+            if (_figures.Contains(shape))
+                throw new InvalidOperationException($"FigureBox already contains {shape.GetType()}.");
+
+            _figures.Add(shape);
+        }
+
+        /// <summary>
+        /// Gets a figure by its index.
+        /// </summary>
+        /// <param name="index">The index of the figure.</param>
+        /// <returns>The requested figure.</returns>
+        /// <exception cref="FigureNotFoundException">Thrown if the index is out of bounds.</exception>
+        public Shape GetFigureByIndex(int index)
+        {
+            if (index < 0 || _figures.Count <= index)
+                throw new FigureNotFoundException($"The requested index ({index}) exceeds the number of items in the collection.");
+
+            return _figures[index];
+        }
+
+        /// <summary>
+        /// Removes a figure by its index.
+        /// </summary>
+        /// <param name="index">The index of the figure to remove.</param>
+        /// <returns>The removed shape.</returns>
         public Shape RemoveFigure(int index)
         {
             var shape = GetFigureByIndex(index);
@@ -59,133 +81,132 @@ namespace Shapes
             return shape;
         }
 
-
         /// <summary>
-        /// Заменить фигуру по номеру
+        /// Replaces a figure at the specified index with a new one.
         /// </summary>
-        /// <param name="index">Номер элемента в коллекции</param>
-        /// <param name="shape">Shape Элемент</param>
+        /// <param name="index">The index to replace.</param>
+        /// <param name="shape">The new shape to place at the index.</param>
+        /// <exception cref="FigureNotFoundException">Thrown if the index is out of bounds.</exception>
         public void ReplaceFigure(int index, Shape shape)
         {
-            if (_figures.Count <= index)
-            {
-                throw new FigureNotFoundException($"The requested number ({index}) exceeds the number of items in the collection!");
-            }
+            if (index < 0 || _figures.Count <= index)
+                throw new FigureNotFoundException($"The requested index ({index}) exceeds the number of items in the collection.");
 
             _figures[index] = shape;
         }
 
         /// <summary>
-        /// Найти фигуру по образцу (эквивалентную по своим характеристикам)
+        /// Finds the first shape with the specified area (within tolerance).
         /// </summary>
-        /// <param name="area">Площадь фигуры</param>
-        /// <returns></returns>
-        public Shape Find(double area)
+        /// <param name="area">The area to search for.</param>
+        /// <returns>A matching shape or null if not found.</returns>
+        public Shape FindByArea(double area)
         {
-            foreach (var item in _figures)
-            {
-                if (item.GetArea().EqualTo(area, Tolerance))
-                {
-                    return item;
-                }
-            }
-            return null;
-        }
-
-
-        /// <summary>
-        /// Показать наличное количество фигур
-        /// </summary>
-        /// <returns>Возвращает количество фигур в коробке</returns>
-        public int Count()
-        {
-            return _figures.Count;
+            return _figures.FirstOrDefault(item => item.GetArea().EqualTo(area, Tolerance));
         }
 
         /// <summary>
-        /// Получить суммарный периметр всех фигур
+        /// Calculates the total perimeter of all shapes in the box.
         /// </summary>
-        /// <returns>Суммарный периметр</returns>
+        /// <returns>The total perimeter.</returns>
         public double GetTotalPerimeter()
         {
             return _figures.Sum(item => item.GetPerimeter());
         }
 
         /// <summary>
-        /// Получить суммарную площадь всех фигур
+        /// Calculates the total area of all shapes in the box.
         /// </summary>
-        /// <returns>Суммарная площадь</returns>
+        /// <returns>The total area.</returns>
         public double GetTotalArea()
         {
             return _figures.Sum(item => item.GetArea());
         }
 
         /// <summary>
-        /// Получить все круги
+        /// Gets all figures of the specified type.
         /// </summary>
-        /// <returns>List<Shape>, иначе null</returns>
-        public List<Shape> GetCircles()
+        /// <typeparam name="T">The type of shape to filter.</typeparam>
+        /// <returns>A list of shapes of the given type.</returns>
+        public List<T> GetFiguresOfType<T>() where T : Shape
         {
-            return _figures.OfType<Circle>().Cast<Shape>().ToList();
+            return _figures.OfType<T>().ToList();
         }
 
         /// <summary>
-        /// Достать все пленочные фигуры
+        /// Gets all shapes made of film material.
         /// </summary>
-        /// <returns>List<Shape>, иначе null</returns>
+        /// <returns>A list of film shapes.</returns>
         public List<Shape> GetFilmShapes()
         {
             return _figures.Where(item => item.Material is Film).ToList();
         }
 
-
         /// <summary>
-        /// Достать все Пластиковые фигуры, которые ни разу не красились 
+        /// Gets all plastic shapes that have not been painted.
         /// </summary>
-        /// <returns>List<Shape>, иначе null</returns>
+        /// <returns>A list of unpainted plastic shapes.</returns>
         public List<Shape> GetNotPaintedPlasticShapes()
         {
-            return _figures.Where(item => item.Material is Plastic && item.Material.IsPainted() == false).ToList();
+            return _figures.Where(item => item.Material is Plastic && !item.Material.IsPainted()).ToList();
         }
+
+        /// <summary>
+        /// Saves all shapes in the box to a file.
+        /// </summary>
+        /// <param name="fileName">The file to save to.</param>
         public void Save(string fileName)
         {
-            _writerService.Save(fileName, _figures);
-        }
-        public void SaveFilmShapes(string fileName)
-        {
-            var shapes = _figures.Where(item => item.Material is Film).ToList();
-            _writerService.Save(fileName, _figures);
-        }
-        public void SavePlasticShapes(string fileName)
-        {
-            var shapes = _figures.Where(item => item.Material is Plastic).ToList();
-            _writerService.Save(fileName, shapes);
-        }
-        public void SavePaperShapes(string fileName)
-        {
-            var shapes = _figures.Where(item => item.Material is Paper).ToList();
-            _writerService.Save(fileName, shapes);
+            _saveLoadService.Save(fileName, _figures);
         }
 
+        /// <summary>
+        /// Saves only shapes made of the specified material type to a file.
+        /// </summary>
+        /// <typeparam name="T">The type of material to filter.</typeparam>
+        /// <param name="fileName">The file to save to.</param>
+        public void SaveByMaterial<T>(string fileName) where T : Material
+        {
+            var shapes = _figures.Where(f => f.Material is T).ToList();
+            _saveLoadService.Save(fileName, shapes);
+        }
 
+        /// <summary>
+        /// Loads shapes from an XML file into the box.
+        /// </summary>
+        /// <param name="fileName">The file to load from.</param>
+        /// <exception cref="InvalidOperationException">Thrown if loaded figures exceed the max capacity.</exception>
         public void LoadFromXml(string fileName)
         {
-            _figures =_writerService.Load(fileName);
+            var loaded = _saveLoadService.Load(fileName);
+            if (loaded.Count > MaxCapacity)
+                throw new InvalidOperationException("Too many figures loaded.");
+
+            _figures.Clear();
+            _figures.AddRange(loaded);
         }
 
+        /// <summary>
+        /// Removes all shapes from the box.
+        /// </summary>
+        public void Clear() => _figures.Clear();
+
+        /// <inheritdoc/>
         public override bool Equals(object obj)
         {
-            return obj is FigureBox box && _figures.Equals(box._figures);
+            return obj is FigureBox box && _figures.SequenceEqual(box._figures);
         }
 
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             return HashCode.Combine(_figures);
         }
+
+        /// <inheritdoc/>
         public override string ToString()
         {
-            return "FigureBox. Count: " + _figures.Count;
+            return $"FigureBox. Count: {_figures.Count}";
         }
     }
-
 }
