60d118c430ad3959ffdbb176d9fdd9a9bf86bb71
asamusenkaEPAM <asamusenka@idbs.com>
2025-07-30 20:31:57 +0200

    New folder for extensions

---
+++ Shapes/Extensions/DoubleExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Shapes.Extensions;
+
+public static class DoubleExtensions
+{
+    public static bool EqualTo(this double a, double b, double tolerance = 0.01)
+        => Math.Abs(a - b) < tolerance;
+}
